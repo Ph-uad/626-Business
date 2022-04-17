9dc33a24fadae42f5831ae7f60e4aa4150b05f74
Ph-uad <temidayo26j@gmail.com>
2022-04-17 13:41:44 +0100

    fixing bugs

--- Asset/script/script.js
+++ Asset/script/script.js
@@ -3,8 +3,6 @@ gsap.registerPlugin(ScrollTrigger)
 let tl = gsap.timeline();
 
 tl.from('.heading-jumbo', {opacity:0, x: -200, duration:1,ease:"sine"})
-tl.from('.header-card__title', {opacity:0, x: -200, duration: .7,ease:"back"})
-tl.from('.header-card__paragraph, .heading-card__list, .pledge-card', {opacity:0,ease:"none", stagger:0.15})
 
 gsap.to( ".service-card", {
     scrollTrigger: {
@@ -22,7 +20,18 @@ gsap.to( ".team-img", {
         start:"centre, center",
         scrub: 'true',
         toggleActions: "restart pause reverse pause"
-    }, duration: .5, x: 80 , ease: "bounce"
+    }, duration: .5,scale: .5, x: 80 , ease: "bounce"
+}, 
+'-=2'
+)
+gsap.to( ".header-hero", {
+    scrollTrigger: {
+        trigger: "body",
+        start:"top, top",
+        scrub: '2',
+        end :"bottom bottom",
+        toggleActions: "restart pause reverse pause"
+    }, duration: .5, y: 2000 , ease: "bounce"
 }, 
 '-=2'
 )
