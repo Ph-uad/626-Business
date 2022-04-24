6962b7a05fa40571d44253276fb5aca1dd90bec3
Ph-uad <temidayo26j@gmail.com>
2022-04-24 16:38:20 +0100

    tweaks

--- Asset/script/script.js
+++ Asset/script/script.js
@@ -2,7 +2,8 @@ gsap.registerPlugin(ScrollTrigger)
 
 let tl = gsap.timeline();
 
-tl.from('.heading-jumbo', {opacity:0, x: -200, duration:1,ease:"sine"})
+tl.from('.heading-jumbo', {height:0 ,opacity:0,y: 20, duration:1,ease:"sine"})
+tl.from('.heading-quadruple', {height:0 ,x: 200, duration:1,ease:"sine"})
 
 gsap.to( ".service-card", {
     scrollTrigger: {
@@ -30,8 +31,10 @@ gsap.to( ".header-hero", {
         start:"top, top",
         scrub: '2',
         end :"bottom bottom",
+        endTrigger:".sector",
+        marker:true,
         toggleActions: "restart pause reverse pause"
-    },  y:1500, scale:.4,opacity: .4, zIndex: 1 , ease: "bounce"
+    },  y:-50, scale:.6,opacity: .4 , ease: "power2"
 }, 
 '-=2'
 )
