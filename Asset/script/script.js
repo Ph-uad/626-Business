95ed50b01913139d261cdcb31601302d0faab364
Ph-uad <temidayo26j@gmail.com>
2022-05-05 00:54:41 +0100

    commit

--- Asset/script/script.js
+++ Asset/script/script.js
@@ -1,7 +1,21 @@
-// gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 let tl = gsap.timeline();
 
-tl.from('.heading-jumbo', {opacity:0,scale: .3, y:-200, duration:1,ease:"sine"})
-tl.from('.heading-quadruple', {opacity: 0 ,x: -200, duration:1,ease:"sine"})
 
+tl.from('.heading-jumbo', {opacity:0, duration:1,ease:"sine"});
+tl.from('.header-paragraph', {opacity: 0 ,x: -200, duration:1,ease:"sine"});
+tl.from('.header-link', {opacity: 0 ,y: -20,ease:"sine"});
+
+
+// let toolMan = gsap.timeline();
+// ScrollTrigger.create({
+//     animation: toolMan,
+//     trigger: ".header-gallery__img",
+//     start: "top top",
+//     end: "2200 100%",
+//     scrub: 1,
+// });
+
+//sun motion 
+toolMan.to(".svg", { x: 120}, 0.00)
