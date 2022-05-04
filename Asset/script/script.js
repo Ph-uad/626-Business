36fb28fc25cba5084e61b1a2d1c281c2e27fb699
Ph-uad <temidayo26j@gmail.com>
2022-05-05 00:54:51 +0100

    commit

--- Asset/script/script.js
+++ Asset/script/script.js
@@ -8,14 +8,14 @@ tl.from('.header-paragraph', {opacity: 0 ,x: -200, duration:1,ease:"sine"});
 tl.from('.header-link', {opacity: 0 ,y: -20,ease:"sine"});
 
 
-// let toolMan = gsap.timeline();
-// ScrollTrigger.create({
-//     animation: toolMan,
-//     trigger: ".header-gallery__img",
-//     start: "top top",
-//     end: "2200 100%",
-//     scrub: 1,
-// });
+let toolMan = gsap.timeline();
+ScrollTrigger.create({
+    animation: toolMan,
+    trigger: ".header-gallery__img",
+    start: "top top",
+    end: "2200 100%",
+    scrub: 1,
+});
 
 //sun motion 
 toolMan.to(".svg", { x: 120}, 0.00)
